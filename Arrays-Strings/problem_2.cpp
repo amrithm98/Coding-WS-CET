d29a43ef42c158e981c630b421d1f8ec95ad30c5
Anand R <anand7t7@gmail.com>
2018-11-17 17:22:46 +0530

    Added Problems

--- Arrays-Strings/problem_2.cpp
+++ Arrays-Strings/problem_2.cpp
@@ -1,3 +1,30 @@
+/*
+Code : Merge two sorted arrays
+
+Given two sorted arrays of Size M and N respectively, merge them into a third array such that the third array is also sorted.
+
+Input Format :
+ Line 1 : Size of first array i.e. M
+ Line 2 : M elements of first array separated by space
+ Line 3 : Size of second array i.e. N
+ Line 2 : N elements of second array separated by space
+
+Output Format :
+M + N integers i.e. elements of third sorted array separated by spaces.
+
+Constraints :
+1 <= M, N <= 10^6
+
+Sample Input :
+5
+1 3 4 7 11
+4
+2 4 6 13
+
+Sample Output :
+1 2 3 4 4 6 7 11 13 
+*/
+
 #include <iostream>
 #include <vector>
 using namespace std;
--- Arrays-Strings/problem_3.cpp
+++ Arrays-Strings/problem_3.cpp
@@ -1,3 +1,29 @@
+/*
+Code : Check Permutation
+
+Given two strings, check if they are permutations of each other. Return true or false.
+Permutation means - length of both the strings should same and should contain same set of characters. Order of characters doesn't matter.
+Note : Input strings contain only lowercase english alphabets.
+
+Input format :
+Line 1 : String 1
+Line 2 : String 2
+
+Sample Input 1 :
+abcde
+baedc
+
+Sample Output 1 :
+true
+
+Sample Input 2 :
+abc
+cbd
+
+Sample Output 2 :
+false
+*/
+
 #include <iostream>
 #include <unordered_map>
 using namespace std;
